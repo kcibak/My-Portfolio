909698ddbab7e96bdf8928427464efb655a2bf6a
Kira Cibak <kcibak@usf.edu>
2026-04-24 10:07:25 -0400

    fixing netlify build

--- Kira-Cibak/astro.config.mjs
+++ Kira-Cibak/astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 // Update `site` with your production domain to unlock correct canonical URLs and sitemap links.
 export default defineConfig({
-	site: 'https://kiracibak.codes'
+	site: 'https://kiracibak.codes',
+	output: 'static'
 });
